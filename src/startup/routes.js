7c476e4331dea63ebe2f2ca221b4479aa941a79f
Iranga Mendis <irangacomz@gmail.com>
2023-07-23 11:30:36 +1000

    Add patient route  for startup

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const patients = require("../routes/patients");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
-
+  app.use("/api/patients", patients);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use(error);
